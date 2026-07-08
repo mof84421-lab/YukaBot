2357e9cb43c56790f274c181a2369f918b13e896
mof84421-lab <mof84421@gmail.com>
2026-07-08 18:55:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,94 @@
 import discord
 from discord.ext import commands
 import os
+import asyncio
+
 from dotenv import load_dotenv
+from config import TOKEN
+
 
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(
-    command_prefix="!",
-    intents=intents
-)
+
+class YukaBot(commands.Bot):
+
+    def __init__(self):
+
+        super().__init__(
+            command_prefix="!",
+            intents=intents
+        )
+
+
+    async def setup_hook(self):
+
+        print("กำลังโหลดระบบ...")
+
+        for file in os.listdir("./cogs"):
+
+            if file.endswith(".py"):
+
+                if file.startswith("_"):
+                    continue
+
+                try:
+
+                    await self.load_extension(
+                        f"cogs.{file[:-3]}"
+                    )
+
+                    print(
+                        f"โหลด {file} สำเร็จ"
+                    )
+
+                except Exception as e:
+
+                    print(
+                        f"โหลด {file} ไม่สำเร็จ:",
+                        e
+                    )
+
+
+        await self.tree.sync()
+
+        print(
+            "Slash Command พร้อมใช้งาน"
+        )
+
+
+bot = YukaBot()
 
 
 @bot.event
 async def on_ready():
-    print(f"ออนไลน์แล้ว: {bot.user}")
 
-    try:
-        synced = await bot.tree.sync()
-        print(f"Sync {len(synced)} commands")
-    except Exception as e:
-        print(e)
+    print(
+        "===================="
+    )
 
+    print(
+        f"Yuka ออนไลน์แล้ว: {bot.user}"
+    )
 
-async def load_extensions():
+    print(
+        f"Server: {len(bot.guilds)}"
+    )
 
-    folders = [
-        "welcome",
-        "moderation",
-        "ticket",
-        "level",
-        "ai",
-        "music",
-        "help"
-    ]
-
-    for file in folders:
-        await bot.load_extension(
-            f"cogs.{file}"
-        )
+    print(
+        "===================="
+    )
 
 
 async def main():
 
     async with bot:
-        await load_extensions()
-        await bot.start(TOKEN)
+
+        await bot.start(
+            TOKEN
+        )
 
 
-import asyncio
 asyncio.run(main())
